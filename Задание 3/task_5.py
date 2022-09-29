4ca4b58c8f9bffb4521bf8929354bdef99cdc21f
AlexandrBespalov <bespalov0710@gmail.com>
2022-09-29 18:50:01 +0300

    Complite ex-3

---
+++ Задание 3/task_5.py
@@ -0,0 +1,12 @@
+from unittest import result
+values = [5, 3, 10, 1, 13, 0.5, 1]
+
+def GetMin(array):
+    result = array[0]
+
+    for i in range(len(array)):
+        if (array[i] < result):
+            result = array[i]
+    return result
+
+print(GetMin(values))
