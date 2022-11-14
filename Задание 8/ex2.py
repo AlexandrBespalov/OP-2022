ed61658a696e8bbcaa7cff725f27b5628eafc8c5
AlexandrBespalov <bespalov0710@gmail.com>
2022-11-14 19:21:56 +0300

    Added change at 8th task

--- Задание 8/ex2.py
+++ Задание 8/ex2.py
@@ -8,7 +8,7 @@ def s(p1, p2):
 ab = int (input('Введите длину стороны AB - '))
 bc = int (input('Введите длину стороны BC - '))
 cd = int (input('Введите длину стороны CD - '))
-ad = int (input('Введите длину стороны AD - '))
+ad = int (input('Введите длину стороны DA - '))
 ac = int (input('Введите длину диагонали AC - '))
  
 p1 = (ab + bc + ac) // 2
