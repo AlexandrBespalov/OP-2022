04e6bc4405e6416cc5e904704104420fb2ed91de
AlexandrBespalov <bespalov0710@gmail.com>
2022-11-14 19:24:47 +0300

    add the 9th task

---
+++ Задание 9/ex1.py
@@ -0,0 +1,12 @@
+from random import randint
+
+m = int(input("Введите вол-во строк "))
+n = int(input("Введите вол-во стоблцов "))
+
+print("Элементы массива:")
+a = [[randint(1,21) for j in range(n)] for i in range(m)]
+for i in range(m):
+    print(a[i], max(a[i]))
+for i in range(n):
+    x=[x[i] for x in a]
+    print(min(x), end = " ")
