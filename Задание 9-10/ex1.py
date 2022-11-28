307f8a863d7fab872169f80b01a1192cce7ebeb0
AlexandrBespalov <bespalov0710@gmail.com>
2022-11-28 07:59:21 +0300

    Change the 10th ex (input-output)

--- Задание 9-10/ex1.py
+++ Задание 9-10/ex1.py
@@ -1,5 +1,9 @@
 from random import randint
 
+with open('ex1_input.txt', 'r') as f:
+    a = [[int(num) for num in line.split(',')] for line in f]
+print(a)
+
 m = int(input("Введите кол-во строк "))
 n = int(input("Введите кол-во стоблцов "))
 
@@ -10,3 +14,6 @@ for i in range(m):
 for i in range(n):
     x=[x[i] for x in a]
     print(min(x), end = " ")
+
+with open("ex1_output.txt", "w") as file:
+    print( file=file, sep=",\n", end="\n") #есть вопросы с выводом в отдельный файл 11-16 строки
