ead292c3ca0dac6af4bfed8cc03f860d8151bba3
AlexandrBespalov <bespalov0710@gmail.com>
2022-12-01 17:28:20 +0300

    Added functions

--- 9-10/ex1.py
+++ 9-10/ex1.py
@@ -1,19 +1,23 @@
 from random import randint
 
-with open('ex1_input.txt', 'r') as f:
-    a = [[int(num) for num in line.split(',')] for line in f]
-print(a)
+def func():
 
-m = int(input("Введите кол-во строк "))
-n = int(input("Введите кол-во стоблцов "))
+    with open('ex1_input.txt', 'r') as f:
+        a = [[int(num) for num in line.split(',')] for line in f]
+    print(a)
 
-print("Элементы массива:")
-a = [[randint(1,21) for j in range(n)] for i in range(m)]
-for i in range(m):
-    print(a[i], max(a[i]))
-for i in range(n):
-    x = [x[i] for x in a]
-    print(min(x), end = " ")
+    m = int(input("Введите кол-во строк "))
+    n = int(input("Введите кол-во стоблцов "))
 
-with open("ex1_output.txt", "w") as file:
-    print( file=file, sep=",\n", end="\n") #есть вопросы с выводом в отдельный файл 11-16 строки
+    print("Элементы массива:")
+    a = [[randint(1,21) for j in range(n)] for i in range(m)]
+    for i in range(m):
+        print(a[i], max(a[i]))
+    for i in range(n):
+        x = [x[i] for x in a]
+        print(min(x), end = " ")
+
+    with open("ex1_output.txt", "w") as file:
+        print( file=file, sep=",\n", end="\n") #есть вопросы с выводом в отдельный файл 11-16 строки
+
+func()
--- 9-10/ex2.py
+++ 9-10/ex2.py
@@ -1,30 +1,34 @@
 from random import shuffle, randint
 
-with open('ex2_input.txt', 'r') as f:
-    с = [[int(num) for num in line.split(',')] for line in f]
-print(с)
+def func():
 
-n = int(input())
-b = [[randint(10,99) for i in range(n)] for j in range(n)]
-shuffle(b)
+    with open('ex2_input.txt', 'r') as f:
+        с = [[int(num) for num in line.split(',')] for line in f]
+    print(с)
 
-for i in b:
-    print(i)
-print()
- 
-a = sum(b, [])
-max1 = max(a[::n+1])
-max2 = max(a[n-1::n-1][:n])
+    n = int(input())
+    b = [[randint(10,99) for i in range(n)] for j in range(n)]
+    shuffle(b)
 
-if max1 > max2:
-    i1 = j1 = a[::n+1].index(max1)
-else:
-    i1 = a[n-1::n-1][:n].index(max2)
-    j1 = n - 1 - i1
-b[n//2][n//2], b[i1][j1] = b[i1][j1], b[n//2][n//2]
- 
-for i in b:
-    print(i)
+    for i in b:
+        print(i)
+    print()
+    
+    a = sum(b, [])
+    max1 = max(a[::n+1])
+    max2 = max(a[n-1::n-1][:n])
 
-with open("ex2_output.txt", "w") as file:
-    print(file=file, sep=",\n", end="\n") #тоже не уверен с выводом
+    if max1 > max2:
+        i1 = j1 = a[::n+1].index(max1)
+    else:
+        i1 = a[n-1::n-1][:n].index(max2)
+        j1 = n - 1 - i1
+    b[n//2][n//2], b[i1][j1] = b[i1][j1], b[n//2][n//2]
+    
+    for i in b:
+        print(i)
+
+    with open("ex2_output.txt", "w") as file:
+        print(file=file, sep=",\n", end="\n") #тоже не уверен с выводом
+
+func()
